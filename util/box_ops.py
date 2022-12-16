9fe0e47e6d68d9662717cf02c24d6ace3de70f7f
SangWonLee <51537341+flowerdolle@users.noreply.github.com>
2022-12-16 15:47:52 +0900

    Update box_ops.py

--- util/box_ops.py
+++ util/box_ops.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 def iou(x1, y1, w1, h1, x2, y2, w2, h2):
@@ -14,14 +15,37 @@ def iou(x1, y1, w1, h1, x2, y2, w2, h2):
 
     x1 = torch.max(box1_x1, box2_x1)
     y1 = torch.max(box1_y1, box2_y1)
-    x2 = torch.max(box1_x2, box2_x2)
-    y2 = torch.max(box1_y2, box2_y2)
+    x2 = torch.min(box1_x2, box2_x2)
+    y2 = torch.min(box1_y2, box2_y2)
 
     intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
     box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
     box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
 
-    return intersection / (box1_area + box2_area - intersection + 1e-6)
+    return intersection / (box1_area + box2_area - intersection + 1e-7)
+
+
+def iou_eval(x1, y1, w1, h1, x2, y2, w2, h2):
+    box1_x1 = x1 - w1 / 2
+    box1_y1 = y1 - h1 / 2
+    box1_x2 = x1 + w1 / 2
+    box1_y2 = y1 + h1 / 2
+
+    box2_x1 = x2 - w2 / 2
+    box2_y1 = y2 - h2 / 2
+    box2_x2 = x2 + w2 / 2
+    box2_y2 = y2 + h2 / 2
+
+    x1 = max(box1_x1, box2_x1)
+    y1 = max(box1_y1, box2_y1)
+    x2 = min(box1_x2, box2_x2)
+    y2 = min(box1_y2, box2_y2)
+
+    intersection = max(0, x2 - x1) * max(0, y2 - y1)
+    box1_area = abs((box1_x2 - box1_x1) * (box1_y2 - box1_y1))
+    box2_area = abs((box2_x2 - box2_x1) * (box2_y2 - box2_y1))
+
+    return intersection / (box1_area + box2_area - intersection + 1e-7)
 
 
 def generalized_iou(pr_bboxes, gt_bboxes):
@@ -68,16 +92,6 @@ def box_3d_xyzxyz_to_cxcyczwhd(x):
          (x1 - x0), (y1 - y0), (z1 - z0)]
     return torch.stack(b, dim=-1)
 
-
-# def box_iou(box1, box2):
-#     area1 = (box1[:, 2] - box1[:, 0]) * (box1[:, 3] - box1[:, 1])
-#     area2 = (box2[:, 2] - box2[:, 0]) * (box2[:, 3] - box2[:, 1])
-#
-#     maxmin = torch.max(box1[:, None, :2], box2[:, :2])
-#     minmax = torch.min(box1[:, None, 2:], box2[:, 2:])
-#
-#     wh =(minmax- maxmin).clamp(min=0)
-#     inter = wh[:]
 
 def box_3d_iou(boxes1, boxes2):
     volume1 = (boxes1[:, 3] - boxes1[:, 0]) * (boxes1[:, 4] - boxes1[:, 1]) * (boxes1[:, 5] - boxes1[:, 2])
